3b6f06bfc840274d3ddaee3e73c8e887fa2159c6
Seren <uemit.seren@gmi-lws06.(none)>
2011-02-22 14:21:44 +0100

    Function addSelectionHandler and addClickGeneHanlder added. variables renamed from GenomeView to GeneViewer and function setDisplayGenes to highlight genes in scatterplot added

--- src/at/gmi/nordborglab/widgets/gwasgeneviewer/client/GWASGeneViewer.java
+++ src/at/gmi/nordborglab/widgets/gwasgeneviewer/client/GWASGeneViewer.java
@@ -1,13 +1,20 @@
 package at.gmi.nordborglab.widgets.gwasgeneviewer.client;
 
+import java.util.ArrayList;
+
 import org.danvk.dygraphs.client.Dygraphs;
 import org.danvk.dygraphs.client.Dygraphs.Options;
 import org.danvk.dygraphs.client.events.HightlightHandler;
+import org.danvk.dygraphs.client.events.SelectHandler;
+import org.danvk.dygraphs.client.events.UnderlayHandler;
 import org.danvk.dygraphs.client.events.UnhighlightHandler;
 import org.danvk.dygraphs.client.events.ZoomHandler;
+import org.danvk.dygraphs.client.events.UnderlayHandler.UnderlayEvent;
 
 import at.gmi.nordborglab.widgets.geneviewer.client.GeneViewer;
 import at.gmi.nordborglab.widgets.geneviewer.client.datasource.DataSource;
+import at.gmi.nordborglab.widgets.geneviewer.client.datasource.Gene;
+import at.gmi.nordborglab.widgets.geneviewer.client.event.ClickGeneHandler;
 import at.gmi.nordborglab.widgets.geneviewer.client.event.ZoomResizeEvent;
 import at.gmi.nordborglab.widgets.geneviewer.client.event.ZoomResizeHandler;
 
@@ -28,31 +35,35 @@ public class GWASGeneViewer extends Composite {
 	interface ScatterGenomeChartUiBinder extends UiBinder<Widget, GWASGeneViewer> {	}
 
 	@UiField Dygraphs scatterChart;
-	@UiField HTMLPanel genomeViewContainer;
+	@UiField HTMLPanel geneViewerContainer;
 	@UiField Label chromosome_label;
-	@UiField GeneViewer genomeView;
+	@UiField GeneViewer geneViewer;
 	
 	//Scatterchart settings
 	private static int DYGRAPHOFFSET = 31;
 	protected double max_value;
 	protected String color;
+	protected String gene_marker_color;
 	protected int pointSize =2;
 	protected int chr_length;
 	protected int scatterChartHeight=200;
 	protected DataTable dataTable;
 	protected boolean isScatterChartLoaded = false;
 	
+	protected SelectHandler selectHandler=null;
+	protected ClickGeneHandler clickGeneHandler = null;
 	
 	//GenomeView settings
 	protected Integer minZoomLevelForGenomeView = 1500000;
-	protected boolean isGenomeViewLoaded = false;
+	protected boolean isGeneViewerLoaded = false;
 	
 	//General settings
 	protected String chromosome;
 	protected int width = 1000;
-	protected int genomeViewHeight = 200;
+	protected int geneViewerHeight = 200;
 	protected DataSource datasource = null;
 	
+	protected ArrayList<Gene> displayGenes = new ArrayList<Gene>();
 	
 	
 
@@ -61,37 +72,53 @@ public class GWASGeneViewer extends Composite {
 		initGenomeView();
 	}
 	
-	public GWASGeneViewer(String chromosome,String color,int width,DataSource datasource) {
+	public GWASGeneViewer(String chromosome,String color,String gene_marker_color,int width,DataSource datasource) {
 		this.chromosome = chromosome;
 		this.color = color;
 		this.width=width;
 		this.datasource = datasource;
+		this.gene_marker_color = gene_marker_color;
 		initWidget(uiBinder.createAndBindUi(this));
 		initGenomeView();
 	}
 	
+	public String getChromosome() {
+		return chromosome;
+	}
 
+	public void addDisplayGene(Gene gene)
+	{
+		if (gene.getChromosome().equals(this.chromosome))
+			displayGenes.add(gene);
+	}
+	
+	public void clearDisplayGenes() 
+	{
+		displayGenes.clear();
+	}
 	
 	private void initGenomeView()
 	{
 		try
 		{
-			genomeView.setLength(chr_length);
-			genomeView.setSize(width - DYGRAPHOFFSET, genomeViewHeight);
-			genomeView.load(new Runnable() {
+			geneViewer.setLength(chr_length);
+			geneViewer.setSize(width - DYGRAPHOFFSET, geneViewerHeight);
+			geneViewer.load(new Runnable() {
 				@Override
 				public void run() {
-					isGenomeViewLoaded = true;
-					genomeView.addZoomResizeHandler(new ZoomResizeHandler() {
+					isGeneViewerLoaded = true;
+					geneViewer.addZoomResizeHandler(new ZoomResizeHandler() {
 						
 						@Override
 						public void onZoomResize(ZoomResizeEvent event) {
 							if (event.stop-event.start > minZoomLevelForGenomeView)
-								genomeViewContainer.setVisible(false);
+								geneViewerContainer.setVisible(false);
 							if (isScatterChartLoaded)
 								scatterChart.setValueRangeX(event.start,event.stop);
 						}
 					});
+					if (clickGeneHandler != null) 
+						geneViewer.addClickGeneHandler(clickGeneHandler);
 				}
 			});
 		}
@@ -100,16 +127,22 @@ public class GWASGeneViewer extends Composite {
 		}
 	}
 	
+	public void refresh() {
+		scatterChart.redraw();
+	}
+	
 	public void draw(DataTable dataTable,double max_value, int chr_length)
 	{
 		this.dataTable = dataTable;
 		this.max_value = max_value;
 		this.chr_length = chr_length;
-		genomeView.setLength(chr_length);
-		genomeView.setChromosome(chromosome);
-		genomeView.setDataSource(datasource);
-		genomeView.setSize(width - DYGRAPHOFFSET, genomeViewHeight);
+		geneViewer.setLength(chr_length);
+		geneViewer.setChromosome(chromosome);
+		geneViewer.setDataSource(datasource);
+		geneViewer.setSize(width - DYGRAPHOFFSET, geneViewerHeight);
 		this.drawScatterChart();
+		if (selectHandler != null)
+			scatterChart.addSelectHandler(selectHandler);
 		scatterChart.addZoomHandler(new ZoomHandler() {
 			
 			@Override
@@ -117,7 +150,7 @@ public class GWASGeneViewer extends Composite {
 				
 				if (event.maxX - event.minX<= minZoomLevelForGenomeView)
 				{
- 					genomeView.updateZoom(event.minX, event.maxX);
+					geneViewer.updateZoom(event.minX, event.maxX);
 					toggleGenomeViewVisible(true);
 				}
 				else
@@ -129,7 +162,7 @@ public class GWASGeneViewer extends Composite {
 			
 			@Override
 			public void onHighlight(HighlightEvent event) {
-				genomeView.setSelectionLine(event.xVal);
+				geneViewer.setSelectionLine(event.xVal);
 			}
 		});
 		
@@ -137,7 +170,7 @@ public class GWASGeneViewer extends Composite {
 			
 			@Override
 			public void onUnhighlight(UnhighlightEvent event) {
-				genomeView.hideSelectionLine();
+				geneViewer.hideSelectionLine();
 			}
 		});
 	}
@@ -147,6 +180,27 @@ public class GWASGeneViewer extends Composite {
 		chromosome_label.setText(chromosome);
 		Dygraphs.Options options = Dygraphs.Options.create();
 		options = setOptions(options);
+		scatterChart.addUnderlayHandler(new UnderlayHandler() {
+			
+			@Override
+			public void onUnderlay(UnderlayEvent event) {
+				for (Gene gene:displayGenes) {
+					if (gene.getChromosome().equals(chromosome))
+					{
+						double left = event.dygraph.toDomXCoord(gene.getStart());
+						double right = event.dygraph.toDomXCoord(gene.getEnd());
+						double length = right - left;
+						if (length < 1)
+						{
+							left = left -0.5;
+							length = 1;
+						}
+						event.canvas.setFillStyle(gene_marker_color);
+						event.canvas.fillRect(left, event.area.getY(), length, event.area.getH());
+					}
+				}
+			}
+		},options);
 		scatterChart.setID(chromosome);
 		scatterChart.draw(dataTable,setOptions(options));
 		isScatterChartLoaded = true;
@@ -171,7 +225,7 @@ public class GWASGeneViewer extends Composite {
 	
 	
 	public void toggleGenomeViewVisible(boolean visible) {
-			genomeViewContainer.setVisible(visible);
+			geneViewerContainer.setVisible(visible);
 	}
 	
 	public void setMinZoomLevelForGenomeView(Integer minZoomLevelForGenomeView) {
@@ -199,7 +253,23 @@ public class GWASGeneViewer extends Composite {
 		this.scatterChartHeight = scatterChartHeight;
 	}
 	
-	public void setGenomeViewHeight(int genomeViewHeight) {
-		this.genomeViewHeight = genomeViewHeight;
+	public void setGeneViewerHeight(int geneViewerHeight) {
+		this.geneViewerHeight = geneViewerHeight;
+	}
+	
+	public void addSelectionHandler(SelectHandler handler) {
+		if (isScatterChartLoaded) {
+				scatterChart.addSelectHandler(handler);
+		}
+		else
+			this.selectHandler = handler;
+	}
+	
+	public void addClickGeneHandler(ClickGeneHandler handler) {
+		if (isGeneViewerLoaded) {
+			geneViewer.addClickGeneHandler(handler);
+	}
+	else
+		this.clickGeneHandler = handler;
 	}
 }
