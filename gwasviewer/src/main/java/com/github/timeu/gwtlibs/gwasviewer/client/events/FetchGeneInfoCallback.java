776351eca42b6b11569655b19497d3db32d83c27
Ümit Seren <uemit.seren@gmail.com>
2016-02-10 16:57:10 +0000

    Creating site for 1.0.0-SNAPSHOT

--- gwasviewer/src/main/java/com/github/timeu/gwtlibs/gwasviewer/client/events/FetchGeneInfoCallback.java
+++
@@ -1,9 +0,0 @@
-package com.github.timeu.gwtlibs.gwasviewer.client.events;
-
-/**
- * Created by uemit.seren on 9/17/15.
- */
-@FunctionalInterface
-public interface FetchGeneInfoCallback {
-    void onFetchGeneInfo(String info);
-}
--- gwasviewer/src/main/java/com/github/timeu/gwtlibs/gwasviewer/client/events/TrackDatasource.java
+++
@@ -1,8 +0,0 @@
-package com.github.timeu.gwtlibs.gwasviewer.client.events;
-
-/**
- * Created by uemit.seren on 10/2/15.
- */
-public interface TrackDatasource {
-
-}
